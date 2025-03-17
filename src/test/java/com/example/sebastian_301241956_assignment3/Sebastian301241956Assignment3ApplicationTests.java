1be467f3cbc70e34e60547cde6978fcf78aa0c69
Sebastian <sebastian_bdavid@hotmail.com>
2025-03-16 20:08:47 -0400

    starter commit

---
+++ src/test/java/com/example/sebastian_301241956_assignment3/Sebastian301241956Assignment3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sebastian_301241956_assignment3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sebastian301241956Assignment3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
