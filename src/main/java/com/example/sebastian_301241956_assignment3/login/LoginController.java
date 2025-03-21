a9c375fe6c292b43d59eca0a97f5cd8c193c6e5b
Sebastian <sebastian_bdavid@hotmail.com>
2025-03-21 14:01:08 -0400

    feat: created LoginController, and LoginRepo

--- src/main/java/com/example/sebastian_301241956_assignment3/login/LoginController.java
+++ src/main/java/com/example/sebastian_301241956_assignment3/login/LoginController.java
@@ -1,4 +1,54 @@
 package com.example.sebastian_301241956_assignment3.login;
 
+import com.example.sebastian_301241956_assignment3.customer.Customer;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import jakarta.servlet.http.HttpSession;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api")
 public class LoginController {
+    @Autowired
+    private LoginRepository loginRepository;
+    @PostMapping("/login")
+    public ResponseEntity<?> loginCustomer(@Valid @RequestBody Customer customer, HttpSession session) {
+        try{
+            // First check if user exists
+            if(loginRepository.findByUsername(customer.getUsername()) == null){
+                return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                        .body("Username does not exist!");
+            }
+            Customer existingCustomer = loginRepository.findByUsername(customer.getUsername());
+            // If this returns false, then the password is incorrect
+            if(!existingCustomer.getPassword().equals(customer.getPassword())) {
+                return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                        .body("Incorrect Password!");
+            }
+
+            // Storing user information in the session
+            session.setAttribute("customerId", existingCustomer.getCustomerId());
+            session.setAttribute("username", existingCustomer.getUsername());
+            // 10 minutes
+            session.setMaxInactiveInterval(60 * 10);
+
+            Map<String, Object> responseBody = new HashMap<>();
+            responseBody.put("status", "success");
+            responseBody.put("message", "Login successful");
+            // Only returning a status message for now
+            return ResponseEntity.ok(responseBody);
+
+
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
 }
--- src/main/java/com/example/sebastian_301241956_assignment3/login/LoginRepository.java
+++ src/main/java/com/example/sebastian_301241956_assignment3/login/LoginRepository.java
@@ -1,4 +1,9 @@
 package com.example.sebastian_301241956_assignment3.login;
 
-public interface LoginRepository {
+import com.example.sebastian_301241956_assignment3.customer.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface LoginRepository extends JpaRepository<Customer, Integer> {
+    Customer findByUsername(String username);
+    Customer findByPassword(String password);
 }
